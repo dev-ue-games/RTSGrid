e4d8ba0e1700a703348e0851f51843150c8e32d6
dev-ue-games <83474161+dev-ue-games@users.noreply.github.com>
2021-05-01 11:38:48 +0300

    Update and rename GridCords to GridCords.h

--- GridCords.h
+++ GridCords.h
@@ -127,27 +127,3 @@ public:
 
 };
 
-
-
-bool UGridCoordinateLibraary::Equal_CoordCoord(FGridCoord A, FGridCoord B)
-{
-	return A == B;
-}
-
-FGridCoord UGridCoordinateLibraary::Multiply_CoordFloat(FGridCoord A, float B)
-{
-	return A * B;
-}
-
-FGridCoord UGridCoordinateLibraary::Multiply_CoordIntFGridCoord(FGridCoord A, int B)
-{
-	return A * B;
-}
-FGridCoord UGridCoordinateLibraary::Multiply_CoordCoord(FGridCoord A, FGridCoord B)
-{
-	return A * B;
-};
-FVector2D UGridCoordinateLibraary::Conv_GridCoordToVector2D(FGridCoord A)
-{
-	return FVector2D(A.Row, A.Col);
-}
