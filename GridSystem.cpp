c5e1914cff6914f694a555b2cbba47980a952e14
dev-ue-games <83474161+dev-ue-games@users.noreply.github.com>
2021-05-07 09:56:11 +0300

    Update GridSystem.cpp

--- GridSystem.cpp
+++ GridSystem.cpp
@@ -1,8 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/ Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "GridSystem.h"
-
+#include "Components/HierarchicalInstancedStaticMeshComponent.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Components/TextRenderComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
 // Sets default values
 AGridSystem::AGridSystem()
 
@@ -14,8 +17,23 @@ AGridSystem::AGridSystem()
 
 
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	RootComp = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = RootComp;
+
+	PreviewGridHISM = CreateDefaultSubobject < UHierarchicalInstancedStaticMeshComponent>(TEXT("PreviewGrid"));
+
+	PreviewGridHISM->SetupAttachment(RootComp);
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh>PlaneMesh(TEXT("StaticMesh'/Engine/BasicShapes/Plane.Plane'"));
+
+		if (PlaneMesh.Succeeded())
+		{
+			PreviewGridHISM->SetStaticMesh(PlaneMesh.Object);
+		}
+
 
 }
 
@@ -48,11 +66,94 @@ TArray<FGridCoord> AGridSystem::GenerateGrid()
 	return GeneratedGrid;
 
 }
+FVector AGridSystem::GetGridOriginRelative()
+{
+	FVector Result;
+	const float CellHalfSize = CellSize * 0.5;
+
+	Result.X = ((GridDimensions.Row * 0.5) * CellSize) - CellHalfSize;
+	Result.Y = ((GridDimensions.Col * 0.5) * CellSize) - CellHalfSize;
+	Result.Z = 0.0;
+	return FVector();
+}
+
+FVector AGridSystem::GetGridOriginWorld()
+{
+	return GetGridOriginRelative() + GetActorLocation() ;
+}
+
+FVector2D AGridSystem::GetGridSize()
+{
+	return FVector2D(GridDimensions.Row * CellSize, GridDimensions.Col * CellSize);
+}
+
+FVector2D AGridSystem::GetGridExtents()
+{
+	return GetGridSize() * 0, 5;
+}
+
+FVector AGridSystem::GetGridRelativeFromWorld(FVector WorldLocation)
+{
+	return WorldLocation - GetActorLocation();
+}
+
+FVector AGridSystem::GetCellCenterFromRelative(FVector RelativeLocation, bool bReturnWorldSpace)
+{
+	int ID;
+	const FGridCoord Coord = GetCoordinateFromRelative(RelativeLocation, ID);
+
+	FVector CellCenter;
+
+	CellCenter.X = Coord.Row * CellSize;
+	CellCenter.Y = Coord.Col * CellSize;
+	CellCenter.Z = 0.0;
+
+	return bReturnWorldSpace ? CellCenter + GetActorLocation() : CellCenter;;
+}
+
+bool AGridSystem::IsInGridBounds(FGridCoord Coordinate)
+{
+	return (Coordinate >= FGridCoord(0.0)&& Coordinate < GridDimensions);
+}
+
+bool AGridSystem::IsClearTile(FGridCoord Coordinate)
+{
+	return !BlockedTiles.Contains(Coordinate);
+}
+
+bool AGridSystem::IsValidLocation(FGridCoord Cordinate)
+{
+	return IsInGridBounds(Cordinate) && IsClearTile(Cordinate);
+}
+
+FGridCoord AGridSystem::GetCoordinateFromRelative(FVector RelativeLocation, int& CellID)
+{
+	FGridCoord Coord;
+	Coord.Col = FMath::RoundToInt(RelativeLocation.Y / CellSize);
+	Coord.Row = FMath::RoundToInt(RelativeLocation.X / CellSize);
+	CellID = GetCellIDFromCoordinate(Coord);
+	return Coord;
+}
+
+FGridCoord AGridSystem::GetCoordinateFromCellID(int ID)
+{
+	FGridCoord Coord;
+	Coord.Col = ID / GridDimensions.Row;
+	Coord.Row = ID % GridDimensions.Row;
+	return Coord;
+}
+
+int AGridSystem::GetCellIDFromCoordinate(FGridCoord Coordinate)
+{
+	return (GridDimensions.Row * Coordinate.Col)+Coordinate.Row;
+}
+
+
 // Called when the game starts or when spawned
 void AGridSystem::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -62,3 +163,83 @@ void AGridSystem::Tick(float DeltaTime)
 
 }
 
+void AGridSystem::GenerateVisualGrid()
+{
+	if (!bShowPreviewGrid == false)
+	{
+		PreviewGridHISM->ClearInstances();
+	}
+	for (auto* text : TextComponents)
+	{
+
+		text->DestroyComponent();
+
+	}
+	TextComponents.Empty();
+
+	PreviewGridHISM->ClearInstances();
+
+	PreviewGridHISM->bAutoRebuildTreeOnInstanceChanges = false;
+
+	for (int i = 0; i < GeneratedGrid.Num(); i++)
+	{
+		FGridCoord CurrentTile = GeneratedGrid[i];
+		if (bShowPreviewGrid && PreviewGridHISM)
+		{
+			const FVector TileLocation = FVector(CurrentTile.Row * CellSize, CurrentTile.Col * CellSize, 0.0);
+			const FTransform T = FTransform(FRotator::ZeroRotator, TileLocation, FVector(CellSize * 0.01));
+
+			PreviewGridHISM->AddInstance(T);
+
+
+		}
+
+		if (bShowTileTextInfo)
+		{
+			UTextRenderComponent* Text = NewObject<UTextRenderComponent>(this);
+			TextComponents.Add(Text);
+
+			if (Text)
+			{
+				Text->RegisterComponent();
+
+
+				Text->HorizontalAlignment = EHorizTextAligment::EHTA_Center;
+				Text->VerticalAlignment = EVerticalTextAligment::EVRTA_TextCenter;
+
+
+				const FVector TileLocation = FVector(CurrentTile.Row * CellSize, CurrentTile.Col * CellSize, 0.0);
+				const FTransform T = FTransform(FRotator (90,0,90), TileLocation, FVector::OneVector);
+
+				Text->AttachToComponent(GetRootComponent(), FAttachmentTransformRules::KeepRelativeTransform);
+				Text->SetRelativeTransform(T);
+
+				FFormatNamedArguments Args;
+				Args.Add("x", CurrentTile.Row);
+				Args.Add("y", CurrentTile.Col);
+				Args.Add("id", i);
+				FText ShowCoords = FText::Format(NSLOCTEXT("gridsys", "DebugTextCoords", "X:{x},Y:{y},\n ID:{id}"), Args);
+				Text->SetText(ShowCoords);
+
+			}
+
+
+
+		}
+
+
+
+	}
+	PreviewGridHISM->bAutoRebuildTreeOnInstanceChanges = true;
+	PreviewGridHISM->BuildTreeIfOutdated(true, true);
+
+	UKismetSystemLibrary::FlushPersistentDebugLines(this);
+	if (bDrawBoundingBox)
+	{
+		
+		UKismetSystemLibrary::DrawDebugBox(this, GetGridOriginWorld(), FVector(GetGridExtents(), 50.0), FLinearColor::Red, FRotator::ZeroRotator, 3000.0, 2.0);
+	}
+	
+
+
+}
